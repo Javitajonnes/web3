91c475c4dd3cd7940703a9a50bdf2af61a415251
Javitajonnes <javitajonnes@gmail.com>
2025-10-14 12:52:36 -0300

    añade admin a ot

--- ot/admin.py
+++ ot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category,MakeOrder
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+class MakeOrderAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(MakeOrder,MakeOrderAdmin)
--- ot/models.py
+++ ot/models.py
@@ -17,15 +17,27 @@ class Category(models.Model):
     def __str__(self):
         return self.name  
     
-    class MakeOrder(models.Model):
-      header=models.CharField(max_length=30, verbose_name="Titulo")
-      detail=models.TextField(verbose_name="Detalle")
-      published=models.DateTimeField(default=now,
+class MakeOrder(models.Model):
+    header=models.CharField(max_length=30, verbose_name="Titulo")
+    detail=models.TextField(verbose_name="Detalle")
+    published=models.DateTimeField(default=now,
                                    verbose_name="F.Orden")
-      image=models.ImageField(upload_to="ordenIMG",
+    image=models.ImageField(upload_to="ordenIMG",
                             null=True,blank=True,
                             verbose_name="Imagen")
-      author=models.ForeignKey(User,on_delete=models.CASCADE,
+    author=models.ForeignKey(User,on_delete=models.CASCADE,
                              verbose_name="Autor")
+    created=models.DateTimeField(auto_now_add=True,
+                                 verbose_name="F.creacion")
+    updated=models.DateTimeField(auto_now=True,
+                                 verbose_name="F.Actualizacion")
+    class Meta():
+        verbose_name="Orden"
+        verbose_name_plural="Ordenes"
+
+    def __str__(self):
+        return self.header
+
+
 
 
