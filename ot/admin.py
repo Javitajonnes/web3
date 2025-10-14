ad0c6b14af555d3fe497bdf6568e60999734d0d2
Javitajonnes <javitajonnes@gmail.com>
2025-10-14 13:13:19 -0300

    se añade busqueda en cpanel

--- ot/admin.py
+++ ot/admin.py
@@ -6,6 +6,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class MakeOrderAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
+    list_display=('header','published','author')
+    ordering=('published','author')
+    search_fields=('header','author__username')
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(MakeOrder,MakeOrderAdmin)
